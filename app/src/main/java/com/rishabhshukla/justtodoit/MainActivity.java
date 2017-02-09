e4633ae11461a1aee115e953c271475261a5f823
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-02-09 21:59:37 +0530

    Begin

---
+++ app/src/main/java/com/rishabhshukla/justtodoit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rishabhshukla.justtodoit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
