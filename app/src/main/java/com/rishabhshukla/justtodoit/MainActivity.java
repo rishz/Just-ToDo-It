cc208c2e5a4502300eb56da1e60b884e170ef614
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-02-10 10:15:51 +0530

    xasj

--- app/src/main/java/com/rishabhshukla/justtodoit/MainActivity.java
+++ app/src/main/java/com/rishabhshukla/justtodoit/MainActivity.java
@@ -3,14 +3,26 @@ package com.rishabhshukla.justtodoit;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.EditText;
+
+import com.rishabhshukla.justtodoit.DB.DBHelper;
+import com.rishabhshukla.justtodoit.DB.Tables.Todos;
+import com.rishabhshukla.justtodoit.Models.ToDo;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et;
     FloatingActionButton fab;
     RecyclerView rv;
+    ToDoRecyclerAdapter todoAdapter;
+    DBHelper dbHelper;
+
+    ArrayList<ToDo> todos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
         et = (EditText) findViewById(R.id.et);
         fab = (FloatingActionButton) findViewById(R.id.btnAdd);
         rv = (RecyclerView) findViewById(R.id.rv);
+        todos = new ArrayList<>();
+        dbHelper = new DBHelper(this);
 
+        todos= Todos.getAllTasks(dbHelper.getReadableDatabase());
+        todoAdapter = new ToDoRecyclerAdapter(this,todos,dbHelper.getWritableDatabase());
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(todoAdapter);
+        refreshTodos();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Todos.addTask(et.getText().toString(),dbHelper.getWritableDatabase());
+                refreshTodos();
+            }
+        });
+
+    }
+
+    void refreshTodos(){
+        todos = Todos.getAllTasks(dbHelper.getReadableDatabase());
+        todoAdapter.updateTodos(todos);
     }
 }
---
+++ app/src/main/java/com/rishabhshukla/justtodoit/ToDoRecyclerAdapter.java
@@ -0,0 +1,82 @@
+package com.rishabhshukla.justtodoit;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.rishabhshukla.justtodoit.Models.ToDo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by rishabhshukla on 10/02/17.
+ */
+
+public class ToDoRecyclerAdapter extends RecyclerView.Adapter<ToDoRecyclerAdapter.ToDoViewHolder>{
+
+    private SQLiteDatabase db;
+    private Context context;
+    private ArrayList<ToDo> toDoArrayList;
+
+    public ToDoRecyclerAdapter(Context ctx, ArrayList<ToDo> todos, SQLiteDatabase sqLiteDatabase) {
+        this.db = sqLiteDatabase;
+        this.context = ctx;
+        this.toDoArrayList = todos;
+    }
+
+    @Override
+    public ToDoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View todoItemView = li.inflate(R.layout.recylerlayout, parent, false);
+
+
+        return new ToDoViewHolder(todoItemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ToDoViewHolder holder, final int position) {
+        holder.tvTask.setText(toDoArrayList.get(position).getTaskName());
+//        holder.tvTask.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Todos.removeTask(toDoArrayList.get(position).getId(),db);
+//                notifyDataSetChanged();
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        if(toDoArrayList==null){
+            return 0;
+        }
+        return toDoArrayList.size();
+    }
+
+    public void updateTodos(ArrayList<ToDo> todos) {
+        this.toDoArrayList = todos;
+        notifyDataSetChanged();
+    }
+
+    public  interface  onDeleteListener{
+        public void onDelete(int position);
+    }
+
+    class ToDoViewHolder extends  RecyclerView.ViewHolder{
+        TextView tvTask;
+        CheckBox checkBox;
+
+        public ToDoViewHolder(View itemView) {
+            super(itemView);
+
+            tvTask = (TextView) itemView.findViewById(R.id.tvDisp);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkBox);
+
+        }
+    }
+}
